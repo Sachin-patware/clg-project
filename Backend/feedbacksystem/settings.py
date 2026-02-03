e014894870032104bdc98cdfcf6e3caea343d0f7
sachin patware <sachinpatware10@gmail.com>
2026-02-03 12:46:26 +0530

    change db config

--- Backend/feedbacksystem/settings.py
+++ Backend/feedbacksystem/settings.py
@@ -130,7 +130,7 @@ WSGI_APPLICATION = 'feedbacksystem.wsgi.application'
 
 DATABASES = {
     "default": {
-        "ENGINE": "mssql",
+        "ENGINE": "django.db.backends.postgresql",
         "NAME": os.getenv("DB_NAME"),
         "USER": os.getenv("DB_USER"),
         "PASSWORD": os.getenv("DB_PASSWORD"),
